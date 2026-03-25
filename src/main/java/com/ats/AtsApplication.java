2f3abbe25c280af35c7507ce48dc5e359dd75fc7
mehar-pa-45 <mehar.pavan77@gmail.com>
2026-03-25 21:15:54 +0530

    Update AtsApplication.java

--- src/main/java/com/ats/AtsApplication.java
+++ src/main/java/com/ats/AtsApplication.java
@@ -2,9 +2,17 @@ package com.ats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AtsApplication {
+public class AtsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AtsApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AtsApplication.class, args);
     }
